bf508c99aefc4f747543bd8a4913e63dfd6fd2de
Eman <cagefury@gmail.com>
2024-03-15 13:25:58 -0700

    Started actual work

--- frontend/svelteflask-frontend/src/App.svelte
+++ frontend/svelteflask-frontend/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Navbar from './components/navbar/Navbar.svelte'
 
   fetch('/api/test')
     .then(response => response.text())
@@ -9,43 +9,21 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <Navbar/>
+  <div class="main-section-container">
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .main-section-container {
+    /* display: flex; */
+    border: solid 1px red;
+    /* background-image: url('./assets/Hero_BG.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat; */
+    width: 100%;
+    height: 200px;
+    background-image: radial-gradient(ellipse 50% 100% at 50% -10%, #7889df50, #778ae800)
   }
 </style>
---
+++ frontend/svelteflask-frontend/src/components/navbar/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import logo from '../../assets/logo.ico';
+</script>
+
+<div class="container">
+    <img src={logo} alt="Main Logo" width="75">
+</div>
+
+<style>
+    .container {
+        display: flex;
+        width: 90%;
+        border: solid 1px blue;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
